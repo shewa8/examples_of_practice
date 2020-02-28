0eb775a8fe937a2c9f2112f93b7c3fa97a38ecae
shewa8 <shevchenko.vlad8@gmail.com>
2020-02-28 18:04:43 +0200

    Learned new features of BDD, add new tests and allure report

--- BDD/features/environment.py
+++ BDD/features/environment.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 def before_scenario(context, scenario):
         context.browser = webdriver.Chrome()
         context.browser.implicitly_wait(10)
-
+        context.browser.maximize_window()
 
 def after_scenario(context, scenario):
         context.browser.quit()
